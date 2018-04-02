ad85c54aa8d2ceea88c4c53c7049057d9d64bead
wayne-tai <wayne.tai@cobinhood.com>
2018-04-02 23:38:54 +0800

    Add more captchas and user session id

--- MiramarTicketMaster/Constants/Environments.swift
+++ MiramarTicketMaster/Constants/Environments.swift
@@ -27,10 +27,14 @@ struct Environments {
     struct LocalInfo {
         static let AppVersion = "5.1.6"
         static let Captcha = "05C65B9C5AEE5E57645AB1B3AFAA8601"
+//        static let Captcha = "502798DF1167DDC46B53D4942C02AC5C"
+//        static let Captcha = "D7E10F08CD0B2A4806D23AFFE1029781"
         static let DeviceId = "adfefa4b7d19f0d69b99f74b86edb03853d9514b"
         static let DeviceType = "1"
         static let DeviceUID = "2FA79CE1A28446CEACBD94D165596AAA"
         static let UserSessionId = "2E2A7E1F714E4FACBDDD9ADD4C5A0426"
+//        static let UserSessionId = "4AEBDED848B84E2AAF5757FF9780C52E"
+//        static let UserSessionId = "EA82D983935E43DC9CFC138C26F7E3BE"
         static let CompleteFrom = 2
         static let CustomerEmail = "wudypig@gmail.com"
         static let CustomerName = "戴 宏偉"
